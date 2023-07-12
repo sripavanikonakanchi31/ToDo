1e50b719eb3967b2a2ced768dce38cee710e1db1
SriPavani <sripavanikonakanchi21@gmail.com>
2023-07-12 21:02:49 +0530

    changes

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 //arthematic operator
 
-let pens = 10;
+
+
   let  books = 20;
 
 let results = pens + books;
 
 console.log(results);
 
+//shift
 
+let arr = [1,5,8,9,46,52]
+
+arr.shift()
+
+console.log(arr) 
+
+//push
+
+// let arr = [1,5,9,8,6,3,4]
+
+arr.push(7)
+
+console.log(arr)
+
+//if
+
+let age = (15,18)
+if (age <= 20)
+
+console.log("we are eligible")
+
+//if else
+
+let marks = 95
+let collage = "chaitanya"
+
+if(marks <=56 && collage === "chaitanya"){
+ console.log("you are a average student")
+}else{
+  console.log("you are a good student")
+}
